481989c8cfd4d2a5ab40fc5f50c7f7751f281319
Ryan Finnie <monstermash2008@gmail.com>
2025-02-24 23:28:18 +1300

    add tasks reducer example from React docs + TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,84 @@
+import { useReducer } from "react";
+import AddTask from "./components/AddTask";
+import TaskList from "./components/TaskList";
+import { Task } from "./types";
+
+let nextId = 3;
+const initialTasks: Task[] = [
+  { id: 0, text: "Philosopher’s Path", done: true },
+  { id: 1, text: "Visit the temple", done: false },
+  { id: 2, text: "Drink matcha", done: false },
+];
+
+export type TaskAction =
+  | { type: "added"; id: number; text: string }
+  | { type: "changed"; task: Task }
+  | { type: "deleted"; id: number };
+
+function tasksReducer(tasks: Task[], action: TaskAction) {
+  switch (action.type) {
+    case "added": {
+      return [
+        ...tasks,
+        {
+          id: action.id,
+          text: action.text,
+          done: false,
+        },
+      ];
+    }
+    case "changed": {
+      return tasks.map((t) => {
+        if (t.id === action.task.id) {
+          return action.task;
+        } else {
+          return t;
+        }
+      });
+    }
+    case "deleted": {
+      return tasks.filter((t) => t.id !== action.id);
+    }
+    default: {
+      throw Error("Unknown action");
+    }
+  }
+}
+
 function App() {
+  const [tasks, dispatch] = useReducer(tasksReducer, initialTasks);
+
+  function handleAddTask(text: string) {
+    dispatch({
+      type: "added",
+      id: nextId++,
+      text: text,
+    });
+  }
+
+  function handleChangeTask(task: Task) {
+    dispatch({
+      type: "changed",
+      task: task,
+    });
+  }
+
+  function handleDeleteTask(taskId: number) {
+    dispatch({
+      type: "deleted",
+      id: taskId,
+    });
+  }
+
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <h1 className="text-4xl font-bold">Hello World</h1>
+    <div className="p-4">
+      <h1 className="text-3xl font-bold">Day off in Kyoto</h1>
+      <AddTask onAddTask={handleAddTask} />
+      <TaskList
+        tasks={tasks}
+        onChangeTask={handleChangeTask}
+        onDeleteTask={handleDeleteTask}
+      />
     </div>
   );
 }

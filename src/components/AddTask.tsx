06082c74e114b3ac2aba587f03a5fdb44653267d
Ryan Finnie <monstermash2008@gmail.com>
2025-02-25 00:00:57 +1300

    use form for adding tasks

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -8,23 +8,26 @@ export default function AddTask({
   onAddTask: (text: string) => void;
 }) {
   const [text, setText] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (text.trim()) {
+      onAddTask(text);
+      setText("");
+    }
+  };
+
   return (
-    <div className="flex gap-2">
+    <form onSubmit={handleSubmit} className="flex gap-2">
       <Input
         className="mb-2"
         placeholder="Add task"
         value={text}
         onChange={(e) => setText(e.target.value)}
       />
-      <Button
-      className="min-w-40"
-        onClick={() => {
-          setText("");
-          onAddTask(text);
-        }}
-      >
+      <Button className="min-w-40" type="submit" disabled={!text.trim()}>
         Add
       </Button>
-    </div>
+    </form>
   );
 }

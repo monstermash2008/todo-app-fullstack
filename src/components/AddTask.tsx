e83a7eac48e300aa4bb6bd00e8209d17a93811b6
Ryan Finnie <monstermash2008@gmail.com>
2025-02-26 19:34:17 +1300

    use shadcn form with react hook form and zod

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,33 +1,53 @@
-import { useState } from "react";
-import { Input } from "./ui/input";
-import { Button } from "./ui/button";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import { Button } from "./ui/button"
+import { Form, FormControl, FormField, FormItem } from "./ui/form"
+import { Input } from "./ui/input"
+
+const formSchema = z.object({
+  task: z.string().min(1, "Task cannot be empty"),
+})
 
 export default function AddTask({
   onAddTask,
 }: {
   onAddTask: (text: string) => void;
 }) {
-  const [text, setText] = useState("");
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      task: "",
+    },
+  })
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (text.trim()) {
-      onAddTask(text);
-      setText("");
-    }
-  };
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    onAddTask(values.task)
+    form.reset()
+  }
 
   return (
-    <form onSubmit={handleSubmit} className="flex gap-2">
-      <Input
-        className="mb-2"
-        placeholder="Add task"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      <Button className="min-w-40" type="submit" disabled={!text.trim()}>
-        Add
-      </Button>
-    </form>
-  );
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex gap-2">
+        <FormField
+          control={form.control}
+          name="task"
+          render={({ field }) => (
+            <FormItem className="flex-1">
+              <FormControl>
+                <Input className="mb-2" placeholder="Add task" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button 
+          className="min-w-40" 
+          type="submit"
+          disabled={!form.formState.isValid}
+        >
+          Add
+        </Button>
+      </form>
+    </Form>
+  )
 }
